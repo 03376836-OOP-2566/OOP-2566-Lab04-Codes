fd0a449ecdfc560b0194ba53a3dc0bec7c7a6b29
koson <567256+koson@users.noreply.github.com>
2024-02-26 03:48:13 +0700

    add codes

---
+++ Lab04_Ex05/Program.cs
@@ -0,0 +1,20 @@
+﻿Nullable<int> a = null;
+int? b = 10;
+
+if (a.HasValue)
+{
+    Console.WriteLine($"a is {b.Value}");
+}
+else
+{
+    Console.WriteLine("a does not have a value");
+}
+
+if (b.HasValue)
+{
+    Console.WriteLine($"b is {b.Value}");
+}
+else
+{
+    Console.WriteLine("b does not have a value");
+}
